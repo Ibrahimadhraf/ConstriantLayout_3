522bc7612c6d679fb16d9d22d8946ec2d7043e8f
Ibrahimadhraf <33137985+Ibrahimadhraf@users.noreply.github.com>
2019-04-06 21:10:02 +0200

    add animation

--- app/src/main/java/com/example/android/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/android/constraintlayout/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.android.constraintlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Group;
+import androidx.constraintlayout.widget.Placeholder;
+import androidx.transition.TransitionManager;
+
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+ private Placeholder mPlaceHolder;
+ private ConstraintLayout mLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPlaceHolder=findViewById(R.id.placeholder3);
+        mLayout=findViewById(R.id.layout);
+
+    }
+
+    public void swapView(View view){
+        TransitionManager.beginDelayedTransition(mLayout);
+        mPlaceHolder.setContentId(view.getId());
+
     }
 }
